c3173cc7b307c769a97d244ff916d13b126f2ebc
Pedro Noronha <128734535+xPedroNoronhax@users.noreply.github.com>
2024-07-16 18:16:18 -0300

    Delete 1-sectionOne directory

--- 1-sectionOne/first-app.js
+++
@@ -1,6 +0,0 @@
-//criando um arquivo com node.Js
-
-
-const fs = require('fs') 
-//passa o nome do arquivo e em seguida o conteudo como parametro
-fs.writeFileSync('hello.txt', 'Hello from node.js')
