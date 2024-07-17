28c5c5f2f2a589073c1ba1f5487cf7db678a5870
Pedro Noronha <pedro_noronha@outlook.com>
2024-07-17 17:10:08 -0300

    add comments

--- 5-dynamic-templating/assignement4/app.js
+++ 5-dynamic-templating/assignement4/app.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+const express = require("express"); // inicia o express
+const bodyParser = require("body-parser"); // inicia o bodyparser
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+const app = express(); // cria nosso app usando express
+app.set("view engine", "ejs");//ativamos nossa view engine e usamos o ejs
+app.set("views", "views");  //views sera encontrado na pasta views
 
-const usersData = require("./routes/add-user");
-const usersRoutes = require("./routes/users");
+const usersData = require("./routes/add-user"); // pegamos nosso middleware de add-user
+const usersPage = require("./routes/users"); // middleware de user
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); // utilizamos o bodyparser para conseguir deixar como string 
 
-app.use(usersData.routes);
-app.use(usersRoutes);
+app.use(usersData.routes); //colocamos no nosso app o add-user
+app.use(usersPage); // colocamos no app o users
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found!</h1>");
+  res.status(404).send("<h1>Page not found!</h1>"); //caso nao tenha nenhuma pagina com aquela url, exibimos isso
 });
 
-app.listen(3000);
+app.listen(3000); // porta que sera executado nosso servidor
--- 5-dynamic-templating/assignement4/routes/add-user.js
+++ 5-dynamic-templating/assignement4/routes/add-user.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const router = express.Router();
+const router = express.Router(); // funcao de roteamento no express
 
-const users = [];
+const users = []; // araray de inicializacao dos users
 
-router.get("/add-user", (req, res, next) => {
-    res.render("add-user");
+router.get("/add-user", (req, res, next) => { //realizmos um get em /add-user
+  res.render("add-user"); //rendereizamos add-user.ejs
 });
 
-router.post("/add-user", (req, res, next) => {
-    users.push({ name: req.body.name });
-    res.redirect("/");
+router.post("/add-user", (req, res, next) => { //faz com que no input que esta em add-usr
+  users.push({ name: req.body.name }); //seja feito um push do conteudo dentro do input
+  console.log(users);
+  res.redirect("/"); //apos o push, direciona para users
 });
 
 exports.routes = router;
